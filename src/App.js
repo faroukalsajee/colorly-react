5238f88ba5384ad2d8992ea64a070443b88eb555
faroukalsajee <faroukalsajee@gmail.com>
2021-07-22 09:52:25 -0700

    use php to store shuffle states and button to access last one

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+
+const API_PATH = "http://localhost:8888";
 
 const colorsList = {
   one: "red",
@@ -13,7 +18,7 @@ const colorsList = {
   eight: "grey",
   nine: "black",
 };
-const initialColors = Object.values(colorsList)
+const initialColors = Object.values(colorsList);
 
 const shufflingArray = (array) => {
   let currentIndex = array.length,
@@ -49,10 +54,90 @@ const App = () => {
     setColors(initialColors);
   };
 
+  /**  show notification for data status from backend.
+  /*   https://prnt.sc/1ed2ni2 */
+  const showToast = ({ type, message }) => {
+    if (type === "info") {
+      toast.info(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else if (type === "error") {
+      toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+  // Save click event to save current status
+  const onClickSave = () => {
+    axios({
+      method: "post",
+      url: `${API_PATH}`,
+      headers: { "content-type": "application/json" },
+      data: {
+        colors,
+      },
+    })
+      .then((res) => {
+        if (res.data.success) {
+          showToast({
+            type: "info",
+            message: "Saved successfully!",
+          });
+        } else {
+          showToast({
+            type: "error",
+            message: res.data.error,
+          });
+        }
+      })
+      .catch((error) => {
+        showToast({
+          type: "error",
+          message: error,
+        });
+      });
+  };
+
+  // Latest status click event to get the latest shuffled colors
+  const onClickLatestStatus = () => {
+    axios
+      .get(`${API_PATH}`)
+      .then((res) => {
+        if (res.data.success) {
+          setColors(res.data.colors);
+        } else {
+          showToast({
+            type: "error",
+            message: res.data.error,
+          });
+        }
+      })
+      .catch((error) => {
+        showToast({
+          type: "error",
+          message: error,
+        });
+      });
+  };
+
   return (
     // colors state will be mapped to render every color as div with correct background color
     <div className="App">
-      <header className="App-header">Colorly</header>
+      <ToastContainer />
+      <header className="App-header">Color Shuffling</header>
 
       <div className="App-body">
         <div className="App-body-container">
@@ -67,12 +152,12 @@ const App = () => {
       </div>
 
       <div className="App-action">
-        <button className="App-action-shuffle" onClick={onClickShuffle}>
+        <button className="App-action-pink-button" onClick={onClickShuffle}>
           Shuffle
         </button>
-        <button className="App-action-reset" onClick={onClickReset}>
-          Reset
-        </button>
+        <button onClick={onClickReset}>Reset</button>
+        <button onClick={onClickSave}>Save</button>
+        <button onClick={onClickLatestStatus}>Latest Status</button>
       </div>
     </div>
   );
